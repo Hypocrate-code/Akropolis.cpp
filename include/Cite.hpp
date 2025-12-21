2508cfdb21a73aa24064559023e7908bc1bf1059
README.mdRussell <russell.rojas-oruna@etu.utc.fr>
2025-12-21 17:37:31 +0100

    fix

--- include/Cite.hpp
+++ include/Cite.hpp
@@ -58,8 +58,8 @@ protected:
 
     static void updateHexSrcFromFan(Hexagone *src, Hexagone *fan);
 
-    
-
+    //liste de tuiles "lac"
+    std::vector<Hexagone*> obtenirTousLesLacs() const; 
 
    
 };
